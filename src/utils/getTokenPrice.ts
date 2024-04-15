0c7a53d7f0037e87e3184180f72363396ac98ab6
Alex <alexmurmaza@gmail.com>
2024-04-14 21:45:02 -0400

    Added birdeye api

---
+++ src/utils/getTokenPrice.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+interface TokenPriceResponse {
+  data: {
+    value: number;
+    updateUnixTime: number;
+    updateHumanTime: string;
+    liquidity: number;
+  };
+  success: boolean;
+}
+
+export default async function getTokenPrice(tokenAddress: string): Promise<number | null> {
+  try {
+    const apiKey = process.env.BIRD_EYE; // Replace 'YOUR_API_KEY' with your actual API key
+    const url = `https://public-api.birdeye.so/defi/price?check_liquidity=1000.25&include_liquidity=true&address=${tokenAddress}`;
+    const headers = {
+      'Accept': 'application/json',
+      'x-chain': 'solana',
+      'X-API-KEY': apiKey
+    };
+
+    const response = await axios.get<TokenPriceResponse>(url, { headers });
+    console.log(response)
+    if (response.data.success) {
+      return response.data.data.value;
+    } else {
+      console.error('Failed to fetch token price:', response.data);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching token price:', error);
+    return null;
+  }
+}
