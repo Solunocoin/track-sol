fcabdafe7543ba94a561ef908c06189bf122f683
igli7 <xhigli7@gmail.com>
2024-04-22 21:52:20 -0400

    fix sol price

--- src/utils/getSolPrice.ts
+++ src/utils/getSolPrice.ts
@@ -1,7 +1,9 @@
 export async function getSolPrice() {
   const url =
     'https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=true';
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    cache: 'no-cache',
+  });
   const data = await response.json();
   const price = data.solana.usd; // Price of Solana in USD
   const change24h = data.solana.usd_24h_change; // 24h price change percentage of Solana
